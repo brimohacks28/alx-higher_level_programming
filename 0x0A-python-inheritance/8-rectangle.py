97697d7b254171b3978c9b2fb878d00eb151d898
brimohacks28@gmail.com <brianmaema28@gmail.com>
2024-02-05 15:12:56 -0800

    Rectangle.8 Correction

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,29 @@
- a class Rectangle based on 7-base_geometry.py.
-
-Attributes:
-    width (int): width of the rectangle.
-    height (int): height of the rectangle.
-"""
+#!/usr/bin/python3
+"""basegeometryclass"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value for integer and positive"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""class rectangle"""
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle"""
-
+    """creates rectangle class"""
     def __init__(self, width, height):
-        """Creates new instances of Rectangle.
-
-        Args:
-            width (int): width of rectangle.
-            height (int): height of rectangle.
-        """
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """initializes rectangle"""
+        if not super().integer_validator("width", width):
+            self.__width = width
+        if not super().integer_validator("height", height):
+            self.__height = height
