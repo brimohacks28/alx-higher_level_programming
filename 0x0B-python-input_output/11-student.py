a3197396bc19da55e3a040bfe3fa2c126164eedb
brimohacks28@gmail.com <brianmaema28@gmail.com>
2024-02-06 10:37:08 -0800

    last

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,14 @@
+ass"""
+
+
+class Student:
+    """class stuff"""
+    def __init__(self, first_name, last_name, age):
+        """initializes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """prints __dict__"""
+        return self.__dict__
