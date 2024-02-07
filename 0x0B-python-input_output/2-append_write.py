a95db71f0cbb3ef034b370e01d2d6460ad4b2d90
brimohacks28@gmail.com <brianmaema28@gmail.com>
2024-02-07 04:09:34 -0800

    Correction file 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""reads lines"""
+"""Module containing the function append_write"""
 
 
-def read_lines(filename="", nb_lines=0):
-    lines = 0
-    with open(filename, mode='r', encoding="utf-8") as f:
-        for line in f:
-            lines += 1
-            print(line, end="")
-            if lines == nb_lines:
-                break
+def append_write(filename="", text=""):
+    """Appends a string to a text file (UTF8) and returns the number
+    of characters added.
+
+    Args:
+        filename (str, optional): name of the file. Defaults to "".
+        text (str, optional): string of text to write to file. Defaults to "".
+
+    Returns:
+        int: number of characters appended to file.
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        """This method returns the number of characters appended to a file."""
+        return f.write(text)
