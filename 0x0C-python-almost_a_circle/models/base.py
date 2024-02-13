82eee15145576b4563ea1ddd4faf8c334ebb764c
brimohacks28@gmail.comn <brianmaema28@gmail.com>
2024-02-13 10:44:13 -0800

    last

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,15 @@
- 
 #!/usr/bin/python3
-""" Module that contains class Base """
+"""class base"""
 import json
-import csv
 import os.path
 
 
 class Base:
-    """ Class Base """
+    """this is inside the class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ Initializes instances """
+        """inits the class"""
         if id is not None:
             self.id = id
         else:
@@ -20,118 +18,70 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ List to JSON string """
-        if list_dictionaries is None or list_dictionaries == "[]":
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """ Save object in a file """
-        filename = "{}.json".format(cls.__name__)
-        list_dic = []
-
-        if not list_objs:
-            pass
-        else:
-            for i in range(len(list_objs)):
-                list_dic.append(list_objs[i].to_dictionary())
-
-        lists = cls.to_json_string(list_dic)
-
-        with open(filename, 'w') as f:
-            f.write(lists)
+        """returns json string"""
+        return json.dumps(list_dictionaries or [])
 
     @staticmethod
     def from_json_string(json_string):
-        """ JSON string to dictionary """
-        if not json_string:
-            return []
-        return json.loads(json_string)
+        """returns list of jsons"""
+        return json.loads(json_string or "[]")
 
     @classmethod
     def create(cls, **dictionary):
-        """ Create an instance """
-        if cls.__name__ == "Rectangle":
-            new = cls(10, 10)
-        else:
-            new = cls(10)
-        new.update(**dictionary)
-        return new
+        """makes instance"""
+        if cls.__name__ is 'Square':
+            newcreate = cls(1)
+        if cls.__name__ is 'Rectangle':
+            newcreate = cls(1, 1)
+        if newcreate:
+            newcreate.update(**dictionary)
+            return newcreate
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to file"""
+        try:
+            jsons = cls.to_json_string([x.to_dictionary() for x in list_objs])
+        except:
+            jsons = '[]'
+        with open(cls.__name__+'.json', 'w', encoding='utf-8') as f:
+            f.write(jsons)
 
     @classmethod
     def load_from_file(cls):
-        """ Returns a list of instances """
-        filename = "{}.json".format(cls.__name__)
-
-        if os.path.exists(filename) is False:
+        if not os.path.isfile(cls.__name__ + '.json'):
             return []
-
-        with open(filename, 'r') as f:
-            list_str = f.read()
-
-        list_cls = cls.from_json_string(list_str)
-        list_ins = []
-
-        for index in range(len(list_cls)):
-            list_ins.append(cls.create(**list_cls[index]))
-
-        return list_ins
+        else:
+            with open(cls.__name__ + '.json', 'r', encoding='utf-8') as f:
+                list_dicts = cls.from_json_string(f.read())
+            return [cls.create(**dic) for dic in list_dicts]
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """ Method that saves a CSV file """
-        filename = "{}.csv".format(cls.__name__)
-
-        if cls.__name__ == "Rectangle":
-            list_dic = [0, 0, 0, 0, 0]
-            list_keys = ['id', 'width', 'height', 'x', 'y']
-        else:
-            list_dic = ['0', '0', '0', '0']
-            list_keys = ['id', 'size', 'x', 'y']
-
-        matrix = []
-
-        if not list_objs:
-            pass
-        else:
-            for obj in list_objs:
-                for kv in range(len(list_keys)):
-                    list_dic[kv] = obj.to_dictionary()[list_keys[kv]]
-                matrix.append(list_dic[:])
-
-        with open(filename, 'w') as writeFile:
-            writer = csv.writer(writeFile)
-            writer.writerows(matrix)
+        import csv
+        try:
+            csvs = [x.to_dictionary() for x in list_objs]
+        except:
+            csvs = '[]'
+        keys = csvs[0].keys()
+        with open(cls.__name__ + '.csv', 'w') as f:
+            writer = csv.DictWriter(f, keys)
+            writer.writeheader()
+            writer.writerows(csvs)
 
     @classmethod
     def load_from_file_csv(cls):
-        """ Method that loads a CSV file """
-        filename = "{}.csv".format(cls.__name__)
-
-        if os.path.exists(filename) is False:
+        import csv
+        if not os.path.isfile(cls.__name__ + '.csv'):
             return []
-
-        with open(filename, 'r') as readFile:
-            reader = csv.reader(readFile)
-            csv_list = list(reader)
-
-        if cls.__name__ == "Rectangle":
-            list_keys = ['id', 'width', 'height', 'x', 'y']
         else:
-            list_keys = ['id', 'size', 'x', 'y']
-
-        matrix = []
-
-        for csv_elem in csv_list:
-            dict_csv = {}
-            for kv in enumerate(csv_elem):
-                dict_csv[list_keys[kv[0]]] = int(kv[1])
-            matrix.append(dict_csv)
-
-        list_ins = []
-
-        for index in range(len(matrix)):
-            list_ins.append(cls.create(**matrix[index]))
-
-        return list_ins                                                                                                                                                                                           
+            with open(cls.__name__ + '.csv', 'r') as f:
+                reader = csv.DictReader(f)
+                csvs = [row for row in reader]
+                for row in csvs:
+                    for key, val in row.items():
+                        try:
+                            row[key] = int(val)
+                        except:
+                            pass
+            return [cls.create(**dic) for dic in csvs]                                                                                                                                                                       
